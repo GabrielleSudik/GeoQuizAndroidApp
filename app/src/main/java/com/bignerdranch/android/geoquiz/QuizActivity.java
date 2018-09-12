bff2aa4cc42eeb93a5af2e499afe36ade9ca3c1b
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-09-12 12:53:43 -0400

    next and true/false buttons work; arrows added as resources

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -36,25 +36,63 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        //int question = mQuestionBank[mCurrentIndex].getTextResId();
+        //mQuestionTextView.setText(question);
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this,
+                       // R.string.correct_toast,
+                       // Toast.LENGTH_SHORT).show();
+                //those lines changed to a method below
+                checkAnswer(true);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this,
+                       // R.string.incorrect_toast,
+                       // Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1)%mQuestionBank.length;
+                //int question = mQuestionBank[mCurrentIndex].getTextResId();
+                //mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    //fyi you originally wrote the same code twice above.
+    //it's more efficient to write it once in a method, here.
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId,Toast.LENGTH_SHORT)
+                .show();
     }
 }
